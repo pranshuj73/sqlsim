aac6dc4baaea66ec100d365c1964716630977ab8
Pranshu Jha <58560660+pranshuj73@users.noreply.github.com>
2025-01-17 17:18:58 +0530

    feat: add dashboard function to fetch orders from db

--- src/sqlsim/dashboard.py
+++ src/sqlsim/dashboard.py
@@ -1,6 +1,6 @@
-import sqlite3
 import os
 
+from .database import get_database_connection, close_database_connection
 from utils.logging import logger
 
 DB_PATH = os.path.join("data", "database.db")
@@ -9,10 +9,15 @@ DB_PATH = os.path.join("data", "database.db")
 def fetch_customers(count=10, all=False) -> list | None:
     """
     Fetch N customers from the database.
+    Parameters: count: int, all: bool
+    count -> Number of customers to fetch (default: 10)
+    all -> Fetch all customers if True, else fetch `count` customers
     """
 
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = get_database_connection()
+        if not conn:
+            return None
         cursor = conn.cursor()
 
         cursor.execute("SELECT * FROM customers")
@@ -22,11 +27,11 @@ def fetch_customers(count=10, all=False) -> list | None:
         else:
             customers = cursor.fetchmany(count)
 
-        conn.close()
+        close_database_connection(conn)
 
         return customers
 
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Error fetching customers: {e}")
         return None
 
@@ -34,10 +39,15 @@ def fetch_customers(count=10, all=False) -> list | None:
 def fetch_products(count=10, all=False) -> list | None:
     """
     Fetch N products from the database.
+    Parameters: count: int, all: bool
+    count -> Number of products to fetch (default: 10)
+    all -> Fetch all products if True, else fetch `count` products
     """
 
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = get_database_connection()
+        if not conn:
+            return None
         cursor = conn.cursor()
 
         cursor.execute("SELECT * FROM products")
@@ -47,10 +57,40 @@ def fetch_products(count=10, all=False) -> list | None:
         else:
             products = cursor.fetchmany(count)
 
-        conn.close()
+        close_database_connection(conn)
 
         return products
 
-    except sqlite3.Error as e:
+    except Exception as e:
         logger.error(f"Error fetching products: {e}")
+        return None
+
+
+def fetch_orders(count=10, all=False) -> list | None:
+    """
+    Fetch N orders from the database.
+    Parameters: count: int, all: bool
+    count -> Number of orders to fetch (default: 10)
+    all -> Fetch all orders if True, else fetch `count` orders
+    """
+
+    try:
+        conn = get_database_connection()
+        if not conn:
+            return None
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM orders")
+        orders = []
+        if all:
+            orders = cursor.fetchall()
+        else:
+            orders = cursor.fetchmany(count)
+
+        close_database_connection(conn)
+
+        return orders
+
+    except Exception as e:
+        logger.error(f"Error fetching orders: {e}")
         return None
